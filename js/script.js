5c4d53b3edf6d3639c02e3937f70bbd695c3f53d
Mark <tractor2554393@yandex.by>
2019-05-01 15:30:58 +0300

    add home-page and contacts page

---
+++ js/script.js
@@ -0,0 +1,259 @@
+let btnSearch = document.querySelector('.search-shop');
+let inputSearch = document.querySelector('.search-input');
+let btnSearchMobile = document.querySelector('.mobile-search-shop');
+let inputSearchMobile = document.querySelector('.mobile-search-input');
+let basketMobile = document.querySelector('.shop-basket-mobile');
+let downNavMobile = document.querySelector('.down-nav-mobile');
+let hamburgerCheckbox = document.getElementById('hambr');
+let navigationBlock = document.querySelector('.mobile-navigation-buttons');
+let cross = document.querySelector('.cross');
+let defaultMenu = document.querySelector('.default-menu');
+let catalogMenu = document.querySelector('.catalog-menu');
+let bossFurniture = document.querySelector('.boss-furniture')
+let warehouses = document.querySelector('.warehouses');
+let warehousesMobile = document.querySelector('.warehouses-mobile');
+let addresses = document.querySelector('.addresses');
+let addressesMobile = document.querySelector('.addressesMobile');
+let phones = document.querySelector('.phones');
+let phonesMobile = document.querySelector('.phones-mobile');
+let modalInputsBlock = document.querySelector('.modal-city-inputs');
+let choiseCity = document.querySelector('.choiseCity');
+let cityList = document.querySelector('.city-list');
+
+function showCatalog() {
+    $(defaultMenu).hide();
+    $(catalogMenu).show();
+};
+
+function showBossFurniture() {
+    $(catalogMenu).hide();
+    $(bossFurniture).show();
+}
+//выбор склада
+$('#warehouseChoice').click(function () {
+    if ($(warehouses).is(":visible")) {
+        $('#warehouseChoice').removeClass('nav-check');
+        $(warehouses).hide();
+    } else {
+        $('#warehouseChoice').addClass('nav-check');
+        $(warehouses).show();
+    }
+});
+
+$(warehouses).click(function (e) {
+    warehouseChoice.textContent = e.target.textContent;
+    $(warehouses).hide();
+});
+
+// Выбор склада мобайл
+$('#warehouseChoiceMobile').click(function () {
+    if ($(warehousesMobile).is(":visible")) {
+        $(warehousesMobile).hide();
+    } else {
+        $(warehousesMobile).show();
+    }
+});
+
+$(warehousesMobile).click(function (e) {
+    warehouseChoiceMobile.textContent = e.target.textContent;
+    $(warehousesMobile).hide();
+});
+//Смена адреса
+$('#changeAddress').click(function () {
+    if ($(addresses).is(":visible")) {
+        $(addresses).hide();
+    } else {
+        $(addresses).show();
+    }
+});
+
+$(addresses).click(function (e) {
+    changeAddress.textContent = e.target.textContent;
+    $(addresses).hide();
+});
+
+//Смена адреса мобайл
+$('#changeAddressMobile').click(function () {
+    if ($(addressesMobile).is(":visible")) {
+        $(addressesMobile).hide();
+    } else {
+        $(addressesMobile).show();
+    }
+});
+
+$(addressesMobile).click(function (e) {
+    changeAddressMobile.textContent = e.target.textContent;
+    $(addressesMobile).hide();
+});
+
+//смена телефона
+$('#changePhones').click(function () {
+    if ($(phones).is(":visible")) {
+        $(phones).hide();
+    } else {
+        $(phones).show();
+    }
+});
+
+$(phones).click(function (e) {
+    changePhones.textContent = e.target.textContent;
+    $(phones).hide();
+});
+
+//смена телефона мобайл
+$('#changeMobilePhone').click(function () {
+    if ($(phonesMobile).is(":visible")) {
+        $(phonesMobile).hide();
+    } else {
+        $(phonesMobile).show();
+    }
+});
+
+$(phonesMobile).click(function (e) {
+    changeMobilePhone.textContent = e.target.textContent;
+    $(phonesMobile).hide();
+});
+
+//поиск
+$(btnSearch).click(function () {
+    $(inputSearch).toggleClass('d-block');
+});
+
+// поиск
+$(btnSearchMobile).click(function () {
+    $(inputSearchMobile).toggleClass('d-block');
+    if ($(window).width() <= '450'){
+        $(downNavMobile).toggleClass('justify-content-end');
+        $(basketMobile).toggleClass('d-none');
+    }
+});
+
+hamburgerCheckbox.addEventListener('click', function () {
+    $(hamburgerCheckbox).change(function(){
+        if ($(this).prop('checked')) {
+            $(navigationBlock).show();
+        } else {
+            $(navigationBlock).hide();
+        }
+    });
+});
+
+function closeMenu() {
+    $(hamburgerCheckbox).prop('checked', false);
+
+    $(catalogMenu).hide();
+    $(bossFurniture).hide();
+
+    $(defaultMenu).show();
+    $(defaultMenu).hide();
+}
+
+// меню в закреп
+jQuery(window).scroll(function() {
+        var theTop = $(window).scrollTop();
+        if(theTop > 160){
+            $(downNavMobile).addClass('fixed');
+        }
+        else{
+            $(downNavMobile).removeClass('fixed');
+        }
+});
+
+jQuery(window).scroll(function() {
+    var theTop = $(window).scrollTop();
+    if(theTop > 120){
+        $('#downNav').addClass('fixed');
+    }
+    else{
+        $('#downNav').removeClass('fixed');
+    }
+});
+
+$(function(){
+    $('#phoneInput').inputmask("+375 (99) 999 -99 -99");
+});
+
+$(function(){
+    $('#phoneInput2').inputmask("+375 (99) 999 -99 -99");
+});
+
+$(function(){
+    $('#modalPhoneInput').inputmask("+375 (99) 999 -99 -99");
+});
+
+function modalClose(elem){
+    elem.hide();
+}
+function modalShow(elem){
+    elem.show();
+}
+
+$(modalInputsBlock).change(function () {
+   if ($("#currentCity").prop("checked")) {
+       $('#currentCityModal').hide();
+   }
+   if ($("#notCurrentCity").prop("checked")) {
+        $(choiseCity).show();
+   } else {
+       $(choiseCity).hide();
+   }
+});
+
+$('#getCallBack').click(function () {
+    $('#callBack').show();
+});
+
+
+$(cityList).click(function () {
+    $('#currentCityModal').hide();
+});
+
+$('#sendCb').click(function () {
+        $('#goodCallBack').show(1000, function(){
+            setTimeout(function(){
+                $('#goodCallBack').hide(500);
+            }, 3000);
+        });
+    }
+);
+
+
+
+// owl carousel
+
+$('.slider-one').owlCarousel({
+    loop:true,
+    nav:false,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+});
+
+$('.slider-two').owlCarousel({
+    loop:true,
+    nav: true,
+    autoplay:  true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        480: {
+            items:2
+        }
+    }
+});
+$('.thrid-slider').owlCarousel({
+    loop:true,
+    nav: true,
+    dots: false,
+    autoplay:  true,
+    responsive:{
+        0:{
+            items:  1
+        }
+    }
+});
+
